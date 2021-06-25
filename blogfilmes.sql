50a8d3614871e439533f8349e59fa58e5bf2fbf0
Jonathan da Silva <jonathansilva15sp@gmail.com>
2021-06-24 23:09:17 -0300

    Banco de Dados do Blog Exercício 24/06/2021

---
+++ blogfilmes.sql
@@ -0,0 +1,14 @@
+use blogfilmes;
+select*from tb_mypostagens;
+insert into tb_mypostagens(filme,descricao)values("Poderoso Chefão II","Adeus fredo" /* ,'1975-14-02*/);
+insert into tb_mypostagens(filme,descricao)values("O Senhor dos Anéis: A Sociedade do Anel",
+"Em uma terra fantástica e única, um hobbit recebe de presente de seu tio um anel mágico e maligno"/*,default,01-01-2001*/);
+insert into tb_mypostagens(filme,descricao)values("O Sistema",
+"Quando a jovem e ambiciosa Sarah Moss é contratada por uma empresa privada de informações, ela é imediatamente enviada a uma perigosa missão como agente disfarçada"
+/*,default,31-05-2013*/);
+insert into tb_mypostagens(filme,descricao)values("O Paizão",
+"Sonny Koufax tem 32 anos de idade e passou toda a sua vida fugindo das responsabilidades."/* ,default,08-10-1999*/);
+insert into tb_mypostagens(filme,descricao)values("Rock IV",
+"Após Apollo Creed, é espancado até a morte por Ivan Dragon Rocky decide ir até a União Soviética para enfrentá-lo e vingar o amigo" /*,default,17-01-1986*/);
+select*from tb_mypostagens;
+ -- drop database blogfilmes;
